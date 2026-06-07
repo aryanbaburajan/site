8980ba84219e91dc39da26c338a70cf1f8010df2
Aryan Baburajan <aryanbaburajan2007@gmail.com>
2026-06-07 14:05:28 +0530

    Simplify site to single home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { satoshi } from "./fonts";
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Aryan Baburajan",
@@ -17,9 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`m-0 p-0 min-h-screen flex justify-center ${satoshi.className}`}>
-        <div className="pointer-events-none fixed inset-0 dot-field" />
-        <div className="w-full max-w-xl px-6 my-20">{children}</div>
-        <Navbar />
+        <div className="w-full max-w-xl px-6 my-40">{children}</div>
       </body>
     </html>
   );
